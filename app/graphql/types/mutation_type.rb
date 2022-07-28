2c102ac662f19998eb31677ee80da18a3f90faa9
Scott Sullivan <scottsullivanltd@gmail.com>
2022-07-28 11:05:26 -0600

    Feat: create tea mutation with tests

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_customer, mutation: Mutations::Customers::CreateCustomer
+    field :create_tea, mutation: Mutations::Teas::CreateTea
   end
 end
